e5be5c018e2f0cc3e99b6d10eab7f1464db1be99
Avadhoot Punde <avadhoot.punde@gmail.com>
2017-05-13 17:42:33 +0530

    Adding basic angular framework and index.html

---
+++ public/js/hsp.module.js
@@ -0,0 +1,6 @@
+(function () {
+'use strict';
+
+angular.module('hsp', ['ui.router']);
+
+})();
---
+++ public/js/hsp.routes.js
@@ -0,0 +1,23 @@
+(function () {
+'use strict';
+
+angular.module('hsp')
+.config(config);
+
+config.$inject = ['$urlRouterProvider', '$stateProvider'];
+function config ($urlRouterProvider, $stateProvider) {
+    $urlRouterProvider.otherwise ('/');
+
+    $stateProvider
+    .state('home', {
+        url: '/',
+        templateUrl: 'views/home.html'
+    })
+    .state('customers', {
+        url: '/customers',
+        templateUrl: 'views/customers.html'
+    });
+
+}
+
+})();
