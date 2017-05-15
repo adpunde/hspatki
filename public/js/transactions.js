8ceb87be8aca6bce48999dd4bbe502ff788a04e1
Avadhoot Punde <avadhoot.punde@gmail.com>
2017-05-15 15:40:47 +0530

    Added transaction display and a add button

--- public/js/transactions.js
+++ public/js/transactions.js
@@ -3,19 +3,19 @@ var transObj = {
         {
             "tin": "27570077768V",
             "date": "15 May 2017",
-            "type": "Sales Tax Return",
+            "type": "SalesTax",
             "filePath": "./trans1.xls"
         },
         {
             "tin": "27410092503V",
             "date": "25 May 2017",
-            "type": "Sales Tax Return",
+            "type": "SalesTax",
             "filePath": "./trans2.xls"
         },
         {
             "tin": "27550292149V",
             "date": "5 May 2017",
-            "type": "Sales Tax Return",
+            "type": "SalesTax",
             "filePath": "./trans3.xls"
         }
     ]
