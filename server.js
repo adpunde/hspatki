45dd233951e8429265d01f03c27ff413a0104dfc
Avadhoot Punde <avadhoot.punde@gmail.com>
2017-05-22 11:06:29 +0530

    Added configuration for heroku as well as local deployment

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var mongoose = require('mongoose');
 var app = express();
 
 //var db = require('./config/db');
-//var conf = require('./config/conf');
+var conf = require('./config/conf');
 
 // Connect to mongodb using mongoose ODM
 //db.connect({ autoRetry: true });
@@ -20,22 +20,26 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // serve static files
 app.use(express.static('public'));
 
-app.set('port', (process.env.PORT || 4000));
-
 // routes
 require('./app/routes')(app);
 
 // start the server
-var server = app.listen(app.get('port'), function () {
-    console.log('Server running');
-});
+var server;
+if (process.env.port)
+    server = app.listen(process.env.port, serverStatus);
+else
+    server = app.listen(conf.port, conf.hostip, serverStatus);
 
-// process.on('SIGINT', function () {
-//     console.log('Stoppping the server');
-//     server.close();
-//     //db.disconnect();
-//     process.exit();
-// });
+function serverStatus() {
+    console.log('Server running: '  + server.address());
+};
+
+process.on('SIGINT', function () {
+    console.log('Stoppping the server');
+    server.close();
+    //db.disconnect();
+    process.exit();
+});
 
 // export app
 module.exports = app;
